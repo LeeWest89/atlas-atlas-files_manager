f0bcd6570a6f2d85be709d1aab035932ff9162f5
LeeWest89 <6683@holbertonstudents.com>
2024-07-09 18:22:49 -0500

    Finished setting up redis.js and db.js testing, they are running properly according to main.js example

---
+++ utils/db.js
@@ -0,0 +1,39 @@
+/*Setup class DBClient*/
+import { MongoClient } from 'mongodb';
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}/${database}`;
+
+class DBClient {
+  /*DBClient Class*/
+  constructor () {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+      if (client) {
+        this.db = client.db(database);
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
+      }
+    });
+  }
+
+  isAlive() {
+    /*returns true when the connection to MongoDB is a success otherwise, false*/
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    /*returns the number of documents in the collection users*/
+    return (await this.users.countDocuments({}));
+  }
+
+  async nbFiles() {
+      /*returns the number of documents in the collection files*/
+    return (await this.files.countDocuments({}));
+  }
+}
+
+
+const dbClient = new DBClient();
+export default dbClient;
---
+++ utils/redis.js
@@ -0,0 +1,41 @@
+/*Setup for RedisClint Class*/
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  /*RedisClient Class*/
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.error(error);
+    });
+
+    this.getA = promisify(this.client.get).bind(this.client);
+    this.setA = promisify(this.client.set).bind(this.client);
+    this.delA = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    /*Checks if Redis is connected*/
+    return (this.client.connected);
+  }
+
+  async get(key) {
+    /*returns the Redis value stored for this key*/
+    return (this.getA(key));
+  }
+
+  async set(key, value, duration) {
+    /*takes a string key, a value and a duration in second as arguments to store it in Redis*/
+    return (this.setA(key, value, 'EX', duration));
+  }
+
+  async del(key) {
+    /*takes a string key as argument and remove the value in Redis for this key*/
+    return (this.delA(key));
+  }
+}
+
+
+const redisClient = new RedisClient();
+export default redisClient;
